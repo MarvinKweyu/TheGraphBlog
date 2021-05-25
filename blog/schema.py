168b6e5f3acf8a14c00d42bf8054a4ebdbfb22d9
MarvinKweyu <mkweyu1@gmail.com>
2021-05-25 18:49:31 +0300

    chore: complete graphql schema

--- blog/schema.py
+++ blog/schema.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from graphene_django import DjangoObjectType
 import graphene
+from django.contrib.auth.models import User
 from blog import models
 
 #  each class below should have a name that ends with `Type`
@@ -8,7 +9,7 @@ from blog import models
 
 class UserType(DjangoObjectType):
     class Meta:
-        model = settings.AUTH_USER_MODEL
+        model = User
 
 class AuthorType(DjangoObjectType):
     class Meta:
@@ -22,6 +23,42 @@ class TagType(DjangoObjectType):
     class Meta:
         model = models.Tag
 
-#  bring together all classes above
-#  add methods to it and indicate how models can 
-# be queried
+"""
+Bring together all classes above add methods to it and indicate how models can 
+be queried
+"""
+
+class Query(graphene.ObjectType):
+
+    all_posts = graphene.List(PostType)
+    author_by_username = graphene.Field(AuthorType, username=graphene.String())
+    post_by_slug = graphene.Field(PostType, slug=graphene.String())
+    posts_by_author = graphene.List(PostType, username=graphene.String())
+    posts_by_tag = graphene.List(PostType, tag=graphene.String())
+
+    def resolve_all_posts(root, info):
+        return(
+            models.Post.objects.prefetch_related("tags").select_related("author").all()
+        )
+
+    def resolve_author_by_username(root, info, username):
+        return (models.Profile.objects.select_related("user").get(user__username=username))
+
+    def resolve_posts_by_author(root, info, username):
+        return ( models.Post.objects.prefetch_related("tags").select_related("author").filter(author__user__username=username))
+
+    def resolve_posts_by_tag(root, info, tag):
+        return (models.Post.objects.prefetch_related("tags").select_related("author").filter(tags__name__iexact=tag))
+
+# schema = graphene.Schema(query=Query)
+
+schema = graphene.Schema(query=Query)
+
+
+
+
+
+
+
+
+
